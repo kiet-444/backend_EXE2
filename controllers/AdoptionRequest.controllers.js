64ef446c82fb8dcd06afb3c23e1714db9f3a04e4
kiet-444 <kietcao444@gmail.com>
2024-11-30 17:44:30 +0700

    adoption rq controller

--- controllers/AdoptionRequest.controllers.js
+++ controllers/AdoptionRequest.controllers.js
@@ -1,30 +1,25 @@
 const AdoptionRequest = require('../models/AdoptionRequest');
 const CartPet = require('../models/CartPet');
-const User = require('../models/User');
-
+const Pet = require('../models/Pet');
 
 const createAdoptionRequest = async (req, res) =>  {
     try {
         const { petId, name, address, phoneNumber, cccd } = req.body;
-
 
         const vaildPhoneNumber = /^\d{10}$/;
         if (!vaildPhoneNumber.test(phoneNumber)) {
             return res.status(400).json({ message: 'Invalid phone number format' });
         }
 
-
         const vaildCCCD = /^\d{12}$/;
         if (!vaildCCCD.test(cccd)) {
             return res.status(400).json({ message: 'Invalid cccd format' });
         }
 
-
         const checkExistAdoptionRequest = await AdoptionRequest.findOne({ pet: petId, user: req.userId, status: 'approved' });
         if (checkExistAdoptionRequest) {
             return res.status(400).json({ message: 'You already have an adoption request for this pet' });
         }
-
 
         const adoptionRequest = new AdoptionRequest({
             pet: petId,
@@ -35,7 +30,6 @@ const createAdoptionRequest = async (req, res) =>  {
             user: req.userId
         });
 
-
         const cartPet = new CartPet({
             userId: req.userId,
             petId
@@ -44,7 +38,6 @@ const createAdoptionRequest = async (req, res) =>  {
 
         await cartPet.save();
 
-
         const savedAdoptionRequest = await adoptionRequest.save();
         res.status(201).json({ message: 'Adoption request created successfully', data: savedAdoptionRequest });
     } catch (error) {
@@ -52,13 +45,11 @@ const createAdoptionRequest = async (req, res) =>  {
     }
 }
 
-
 // get All
 const getAllAdoptionRequest = async (req, res) => {
     try {
         const userId = req.userId;
         const userRole = req.userRole;
-
 
         if(userRole === 'admin') {
             const adoptionRequests = await AdoptionRequest.find();
@@ -72,14 +63,24 @@ const getAllAdoptionRequest = async (req, res) => {
     }
 }
 
+// get by id
+const getAdoptionRequestById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const adoptionRequest = await AdoptionRequest.findById(id);
+        if (!adoptionRequest) {
+            return res.status(404).json({ message: 'Adoption request not found' });
+        }
+        res.status(200).json({ data: adoptionRequest, message: 'Adoption request retrieved successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to get adoption request', error });
+    }
+}
 
 // Update status of an adoption request
 const updateStatusAdoptionRequest = async (req, res) => {
     try {
         const { id, status: newStatus } = req.body;
-
-        console.log(id, newStatus);
-
 
         const adoptionRequest = await AdoptionRequest.findByIdAndUpdate(
             { _id: id },
@@ -87,15 +88,11 @@ const updateStatusAdoptionRequest = async (req, res) => {
             { new: true }
         );
 
-
         if (!adoptionRequest) {
             return res.status(404).json({ message: 'Adoption request not found' });
         }
 
-
         const { pet: petId, user: userId } = adoptionRequest;
-
-        console.log(petId, userId);
 
         if (newStatus === 'approved') {
             await CartPet.findOneAndUpdate(
@@ -106,7 +103,7 @@ const updateStatusAdoptionRequest = async (req, res) => {
                 { _id: petId },
                 { $set: { status: 'adopted' } }
             )
-        
+
         } else if (newStatus === 'rejected') {
             await CartPet.findOneAndUpdate(
                 { userId, petId },
@@ -127,9 +124,6 @@ const updateStatusAdoptionRequest = async (req, res) => {
             )
         }
 
-        await adoptionRequest.save();
-
-
         res.status(200).json({ message: 'Adoption request status updated successfully', data: adoptionRequest });
     } catch (error) {
         res.status(500).json({ message: 'Failed to update adoption request status', error });
@@ -137,7 +131,25 @@ const updateStatusAdoptionRequest = async (req, res) => {
 }
 
 
+const countDay = async (req, res) => {
+    try {
+        const adoptionRequests = await AdoptionRequest.find();
+        adoptionRequests.forEach(async (request) => {
+            if(request.status === 'approved' || request.status === 'rejected') {
+                request.countDay += 1;
+                await request.save();
+            }
+            if(request.countDay === 7) {
+                await request.remove();
+            }
+        })
+        res.status(200).json({ message: 'Count day successfully', data: adoptionRequests });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to count day', error });
+    }
+}
+
 
 module.exports = {
-    createAdoptionRequest, getAllAdoptionRequest, updateStatusAdoptionRequest
+    createAdoptionRequest, getAllAdoptionRequest, updateStatusAdoptionRequest, countDay, getAdoptionRequestById
 }
